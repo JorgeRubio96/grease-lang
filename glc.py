5d20b0d8c6c48393b2c83508ace532bd83a7bc27
Javier Meza <javenmeza@gmail.com>
2018-03-27 13:22:59 -0600

    Adds module defenition

--- glc.py
+++ glc.py
@@ -2,9 +2,7 @@ import sys
 from scanner import grease_lexer
 from parser import grease_parser
 
-
-
-def main():
+def _main():
     data = ''
 
     if len(sys.argv) > 1:
@@ -14,7 +12,10 @@ def main():
         for line in sys.stdin:
             data = data + line
 
+    compile(data)
+
+def compile(data):
     result = grease_parser.parse(data,lexer=grease_lexer, debug=False, tracking=True)
 
 if __name__ == '__main__':
-    main()
+    _main()
